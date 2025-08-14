c619bdc97aa7b02104463b4e75d6e9538ed29e58
sandeep908 <sandeep908singh908@gmail.com>
2025-08-14 23:54:45 +0530

    Setup file is committed

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages , setup
+from typing import List
+
+HYPHON_E_DOT = '-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function returns the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements= [req.replace("\n", "") for req in requirements]
+        
+        if HYPHON_E_DOT in requirements:
+            requirements.remove(HYPHON_E_DOT)
+        
+    return requirements
+    
+
+
+
+setup(
+    name="ds_project",
+    version="0.0.1",
+    author="Sandeep",
+    author_email="sandeep908singh908@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
