5e6c6ffd3aa42ce46bae7db1d9ca8fc9cfd0cae5
sandeep908 <sandeep908singh908@gmail.com>
2025-08-20 22:42:03 +0530

    The model is trained

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from src.ds_project.components.data_ingestion import Dataingestion
 from src.ds_project.components.data_ingestion import DataIngestionConfig
 from src.ds_project.components.data_transformation import DataTransformation, DataTransformtionConfig
 
+from src.ds_project.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 if __name__=="__main__":
     logging.info("The execution has started")
     
@@ -15,7 +17,12 @@ if __name__=="__main__":
         
         data_transformation_config = DataTransformtionConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path , test_data_path)
+        train_arr, test_arr , _ = data_transformation.initiate_data_transformation(train_data_path , test_data_path)
+        
+        # Model training code
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr , test_arr))
+        
         
     except Exception as e:
         logging.info("Custom Exception")
